0027356410649c3305d404f9923f73f72b7ac2b2
Andrius <andrius.urbanavicius@aktkc.com>
2019-07-04 13:36:29 +0300

    Rotated card for vertical hand view

--- app/src/main/java/com/example/cards/views/HandViewV.java
+++ app/src/main/java/com/example/cards/views/HandViewV.java
@@ -1,9 +1,13 @@
 package com.example.cards.views;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.util.AttributeSet;
 
 import com.example.cards.R;
+import com.example.cards.domain.Card;
+import com.example.cards.domain.Player;
+import com.example.cards.domain.PlayerState;
 
 import androidx.annotation.Nullable;
 
@@ -24,5 +28,22 @@ public class HandViewV extends HandView {
     @Override
     int getLayoutId() {
         return R.layout.hand_view_vertical;
+    }
+
+    @Override
+    public void update(Player player) {
+        llCards.removeAllViews();
+        for (Card card : player.getHand()) {
+            CardViewRotated cardView = new CardViewRotated(getContext(), card, player);
+            llCards.addView(cardView);
+        }
+        if (player.getState() == PlayerState.ATTACK) {
+            setBackgroundColor(Color.RED);
+        } else if (player.getState() == PlayerState.DEFEND) {
+            setBackgroundColor(Color.BLUE);
+        } else {
+            setBackgroundColor(getResources().getColor(R.color.gray));
+        }
+
     }
 }
